4e431cd4b8a85ba61a6c752220a3e62c546c41af
Nayuki Minase <nayuki@eigenstate.org>
2015-08-13 17:58:43 +0000

    Int256Math, Int256MathTest: Added field integer operations, added test suite.

--- java/Int256Math.java
+++ java/Int256Math.java
@@ -136,6 +136,154 @@ public final class Int256Math {
 	}
 	
 	
+	/*---- Field arithmetic methods ----*/
+	
+	// Computes x = (x + y) mod prime. Constant-time with respect to both values.
+	public static void fieldAdd(int[] x, int xOff, int[] y, int yOff) {
+		checkFieldInt(x, xOff);
+		checkFieldInt(y, yOff);
+		checkOverlap(x, xOff, y, yOff);
+		
+		int c = uintAdd(x, xOff, y, yOff, 1);  // Perform addition
+		assert((c >>> 1) == 0);
+		uintSubtract(x, xOff, FIELD_MODULUS, 0, c | (lessThan(x, xOff, FIELD_MODULUS, 0) ^ 1));  // Conditionally subtract modulus
+	}
+	
+	
+	// Computes x = (x - y) mod prime. Constant-time with respect to both values.
+	public static void fieldSubtract(int[] x, int xOff, int[] y, int yOff) {
+		checkFieldInt(x, xOff);
+		checkFieldInt(y, yOff);
+		checkOverlap(x, xOff, y, yOff);
+		
+		int b = uintSubtract(x, xOff, y, yOff, 1);  // Perform subtraction
+		assert((b >>> 1) == 0);
+		uintAdd(x, xOff, FIELD_MODULUS, 0, b);  // Conditionally add modulus
+	}
+	
+	
+	// Computes x = (x * 2) mod prime. Constant-time with respect to the value.
+	public static void fieldMultiply2(int[] x, int xOff) {
+		checkFieldInt(x, xOff);
+		int c = uintShiftLeft1(x, xOff);
+		assert((c >>> 1) == 0);
+		uintSubtract(x, xOff, FIELD_MODULUS, 0, c | (lessThan(x, xOff, FIELD_MODULUS, 0) ^ 1));  // Conditionally subtract modulus
+	}
+	
+	
+	// Computes x = (x * 3) mod prime. Requires 8 words of temporary space. Constant-time with respect to the value.
+	public static void fieldMultiply3(int[] x, int xOff, int[] temp, int tempOff) {
+		checkFieldInt(x, xOff);
+		checkArray(temp, tempOff);
+		System.arraycopy(x, xOff, temp, tempOff, NUM_WORDS);
+		fieldMultiply2(x, xOff);
+		fieldAdd(x, xOff, temp, tempOff);
+	}
+	
+	
+	// Computes x = x^2 mod prime. Requires 72 words of temporary space. Constant-time with respect to the value.
+	public static void fieldSquare(int[] x, int xOff, int[] temp, int tempOff) {
+		checkFieldInt(x, xOff);
+		fieldMultiply(x, xOff, x, xOff, temp, tempOff);
+	}
+	
+	
+	// Computes x = (x * y) mod prime. Requires 72 words of temporary space. Constant-time with respect to both values.
+	public static void fieldMultiply(int[] x, int xOff, int[] y, int yOff, int[] temp, int tempOff) {
+		checkFieldInt(x, xOff);
+		checkFieldInt(y, yOff);
+		checkArray(temp, tempOff);
+		assert temp.length - tempOff >= 9 * NUM_WORDS;
+		
+		// Compute raw product of this.value * other.value
+		int product0Off = tempOff + 0 * NUM_WORDS;
+		{
+			long carry = 0;
+			int i;
+			for (i = 0; i < NUM_WORDS * 2 - 1; i++) {
+				long sum = carry;
+				int c = 0;
+				if (i < NUM_WORDS) {
+					for (int j = 0; j <= i; j++) {
+						long prod = (x[xOff + j] & LONG_MASK) * (y[yOff + i - j] & LONG_MASK);
+						sum += prod;
+						c += lessThan(sum, prod);
+					}
+				} else {
+					for (int j = NUM_WORDS - 1; j >= 0 && i - j < NUM_WORDS; j--) {
+						long prod = (x[xOff + i - j] & LONG_MASK) * (y[yOff + j] & LONG_MASK);
+						sum += prod;
+						c += lessThan(sum, prod);
+					}
+				}
+				assert(0 <= c && c <= NUM_WORDS);
+				temp[product0Off + i] = (int)sum;
+				carry = (long)c << 32 | sum >>> 32;
+			}
+			temp[product0Off + i] = (int)carry;
+			assert((carry >>> 32) == 0);
+		}
+		
+		// Barrett reduction algorithm begins here.
+		// Multiply by floor(2^512 / MODULUS), which is 2^256 + 2^32 + 0x3D1
+		int product1Off = tempOff + 2 * NUM_WORDS;
+		{
+			int carry = 0;
+			for (int i = 0; i < NUM_WORDS * 3; i++) {
+				long sum = carry;
+				if (i < NUM_WORDS * 2)
+					sum += (temp[product0Off + i] & LONG_MASK) * 0x3D1;
+				if (i >= 1 && i < NUM_WORDS * 2 + 1)
+					sum += temp[product0Off + i - 1] & LONG_MASK;
+				if (i >= NUM_WORDS)
+					sum += temp[product0Off + i - NUM_WORDS] & LONG_MASK;
+				temp[product1Off + i] = (int)sum;
+				carry = (int)(sum >>> 32);
+				assert(0 <= carry && carry <= 0x3D3);
+			}
+			assert(carry == 0);
+		}
+		
+		// Virtually shift right by 512 bits, then multiply by MODULUS. Note that MODULUS = 2^256 - 2^32 - 0x3D1
+		int p1Shift = product1Off + NUM_WORDS * 2;
+		int product2Off = tempOff + 5 * NUM_WORDS;
+		{
+			int borrow = 0;
+			for (int i = 0; i < NUM_WORDS * 2; i++) {
+				long diff = -borrow;
+				if (i < NUM_WORDS)
+					diff -= (temp[p1Shift + i] & LONG_MASK) * 0x3D1;
+				if (i >= 1 && i < NUM_WORDS + 1)
+					diff -= temp[p1Shift + i - 1] & LONG_MASK;
+				if (i >= NUM_WORDS)
+					diff += temp[p1Shift + i - NUM_WORDS] & LONG_MASK;
+				temp[product2Off + i] = (int)diff;
+				borrow = -(int)(diff >>> 32);
+				assert(0 <= borrow && borrow <= 0x3D3);
+			}
+			assert(borrow == 0);
+		}
+		
+		// Compute product0 - product2
+		int differenceOff = tempOff + 7 * NUM_WORDS;  // We use 9 words but allocate 16
+		{
+			int borrow = 0;
+			for (int i = 0; i < NUM_WORDS + 1; i++) {
+				long diff = (temp[product0Off + i] & LONG_MASK) - (temp[product2Off + i] & LONG_MASK) - borrow;
+				temp[differenceOff + i] = (int)diff;
+				borrow = -(int)(diff >>> 32);
+				assert((borrow >>> 1) == 0);
+			}
+		}
+		
+		// Final conditional subtraction
+		System.arraycopy(temp, differenceOff, x, xOff, NUM_WORDS);
+		int enable = (equalTo(temp[differenceOff + NUM_WORDS], 0) & lessThan(x, xOff, FIELD_MODULUS, 0)) ^ 1;
+		uintSubtract(x, xOff, FIELD_MODULUS, 0, enable);
+	}
+	
+	
+	
 	/*---- Miscellaneous methods ----*/
 	
 	// Copies the value y into x iff enable is 1.
@@ -200,6 +348,19 @@ public final class Int256Math {
 	
 	/*---- Helper methods ----*/
 	
+	// Returns 1 if x == y, otherwise 0.
+	private static int equalTo(int x, int y) {
+		int z = x ^ y;
+		return ~(z | -z) >>> 31;
+	}
+	
+	
+	// Returns 1 if x < y, otherwise 0.
+	private static int lessThan(long x, long y) {
+		return (int)(((~x & y) | ((~x ^ y) & (x - y))) >>> 63);
+	}
+	
+	
 	private static void checkEnable(int en) {
 		assert (en >>> 1) == 0;
 	}
@@ -212,6 +373,11 @@ public final class Int256Math {
 		assert x != y || xOff != yOff;
 	}
 	
+	private static void checkFieldInt(int[] arr, int off) {
+		checkArray(arr, off);
+		assert lessThan(arr, off, FIELD_MODULUS, 0) == 1;
+	}
+	
 	
 	/*---- Constants ----*/
 	
@@ -220,5 +386,6 @@ public final class Int256Math {
 	
 	private static final int[] ZERO = {0, 0, 0, 0, 0, 0, 0, 0};
 	private static final int[] ONE  = {1, 0, 0, 0, 0, 0, 0, 0};
+	private static final int[] FIELD_MODULUS = {0xFFFFFC2F, 0xFFFFFFFE, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF};
 	
 }
