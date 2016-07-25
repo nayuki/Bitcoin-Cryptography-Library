67046f5c08ee6ad7a2791b3a7fb735f050b7ba32
Project Nayuki <me@nayuki.io>
2016-07-25 21:06:32 +0000

    Base58Check.cpp: Rearranged functions, added section headings.

--- cpp/Base58Check.cpp
+++ cpp/Base58Check.cpp
@@ -14,6 +14,9 @@
 #include "Utils.hpp"
 
 
+
+/*---- Public and private functions for bytes-to-Base58 conversion ----*/
+
 void Base58Check::pubkeyHashToBase58Check(const uint8_t pubkeyHash[RIPEMD160_HASH_LEN], char outStr[35]) {
 	assert(pubkeyHash != nullptr && outStr != nullptr);
 	uint8_t toEncode[1 + RIPEMD160_HASH_LEN + 4] = {};
@@ -73,6 +76,42 @@ void Base58Check::bytesToBase58Check(uint8_t *data, size_t dataLen, char *outStr
 }
 
 
+bool Base58Check::isZero(const uint8_t *x, size_t len) {
+	assert(len == 0 || x != nullptr);
+	for (size_t i = 0; i < len; i++) {
+		if (x[i] != 0)
+			return false;
+	}
+	return true;
+}
+
+
+uint8_t Base58Check::mod58(const uint8_t *x, size_t len) {
+	assert(len == 0 || x != nullptr);
+	uint_fast16_t sum = 0;
+	for (size_t i = 0; i < len; i++)
+		sum = ((sum * 24) + x[i]) % 58;  // Note: 256 % 58 = 24
+	return static_cast<uint8_t>(sum);
+}
+
+
+void Base58Check::divide58(const uint8_t *x, uint8_t *y, size_t len) {
+	assert(x != nullptr && y != nullptr);
+	memset(y, 0, len);
+	uint_fast16_t dividend = 0;
+	for (size_t i = 0; i < len * 8; i++) {  // For each output bit
+		dividend = (dividend << 1) | ((x[i >> 3] >> (7 - (i & 7))) & 1);  // Shift next input bit into right side
+		if (dividend >= 58) {
+			dividend -= 58;
+			y[i >> 3] |= 1 << (7 - (i & 7));
+		}
+	}
+}
+
+
+
+/*---- Public and private functions for Base58-to-bytes conversion ----*/
+
 bool Base58Check::privateKeyFromBase58Check(const char wifStr[53], Uint256 &outPrivKey) {
 	// Preliminary checks - these prevent arithmetic overflow and let us not need to handle leading zero bytes
 	assert(wifStr != nullptr);
@@ -108,39 +147,6 @@ bool Base58Check::privateKeyFromBase58Check(const char wifStr[53], Uint256 &outP
 }
 
 
-bool Base58Check::isZero(const uint8_t *x, size_t len) {
-	assert(len == 0 || x != nullptr);
-	for (size_t i = 0; i < len; i++) {
-		if (x[i] != 0)
-			return false;
-	}
-	return true;
-}
-
-
-uint8_t Base58Check::mod58(const uint8_t *x, size_t len) {
-	assert(len == 0 || x != nullptr);
-	uint_fast16_t sum = 0;
-	for (size_t i = 0; i < len; i++)
-		sum = ((sum * 24) + x[i]) % 58;  // Note: 256 % 58 = 24
-	return static_cast<uint8_t>(sum);
-}
-
-
-void Base58Check::divide58(const uint8_t *x, uint8_t *y, size_t len) {
-	assert(x != nullptr && y != nullptr);
-	memset(y, 0, len);
-	uint_fast16_t dividend = 0;
-	for (size_t i = 0; i < len * 8; i++) {  // For each output bit
-		dividend = (dividend << 1) | ((x[i >> 3] >> (7 - (i & 7))) & 1);  // Shift next input bit into right side
-		if (dividend >= 58) {
-			dividend -= 58;
-			y[i >> 3] |= 1 << (7 - (i & 7));
-		}
-	}
-}
-
-
 void Base58Check::addUint8(uint8_t *x, uint8_t y, size_t len) {
 	assert(len >= 1 && x != nullptr);
 	uint_fast16_t carry = 0;
@@ -171,6 +177,9 @@ void Base58Check::multiply58(uint8_t *x, size_t len) {
 	}
 }
 
+
+
+/*---- Miscellaneous definitions ----*/
 
 Base58Check::Base58Check() {}
 
